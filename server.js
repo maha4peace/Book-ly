4df5b4eb7e10efe8a9e9980ef50c3aa8a6253d73
Maha <maha.ahmed4peace@gmail.com>
2024-02-27 23:16:13 -0500

    setting up post route handler to create notes

--- server.js
+++ server.js
@@ -3,19 +3,36 @@ require("dotenv").config() ;
 
 //importing dependencies 
 const express = require("express")
-
-//connecting to database
 require("./config/databaseDB")
+const Note = require('./models/note')
 
 
 //create an express app 
 const app = express() ; 
 
+//configure express app to read json
+app.use(express.json()) ; 
 
 //Routing
 app.get("/", (req, res) => {
     res.json({ hello: "world"})
 }) ; 
+
+app.post('/notes', async (req, res) => {
+    //Get the sent in data off request body
+    const title = req.body.title; 
+    const body = req.body.body;
+
+    //Create a note with it
+    const note = await Note.create({
+       title: title,
+       body: body, 
+    })
+
+    //respond with the new note 
+    res.json({note:note})
+
+})
 
 //Start our server
 const port = process.env.PORT || 3001;
